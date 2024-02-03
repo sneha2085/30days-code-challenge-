bb8fd8db2403fcdcd03634c81ca2e98c0a82ff96
sneha2085 <147432777+sneha2085@users.noreply.github.com>
2024-02-03 10:50:24 +0530

    Update

---
+++ day3p.java
@@ -0,0 +1,14 @@
+import java.util.*;<br>
+class HelloWorld<br>
+{<br>   
+ public static void main(String[] args)<br>    
+{<br>        
+Scanner sc = new Scanner (System.in);<br>        System.out.println("Enter an integer");<br>       
+ int a = sc.nextInt();<br>        if(a%2==0)<br>        
+{<br>           
+ System.out.println("The number is even");<br>       
+ }<br>        
+else<br>      
+  System.out.println("The number is odd");<br>   
+ }<br>
+}<br>
