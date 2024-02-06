fe1d4fdf1ff78384ded2c47886b8e67d02480145
sneha2085 <147432777+sneha2085@users.noreply.github.com>
2024-02-06 15:53:02 +0530

    Update

---
+++ day6p.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;<br>
+class array<br>
+{<br>  
+  public static void main(String[] args)<br>    
+{<br>      
+  int[] array = {2,4,6,8};<br>        System.out.print(Arrays.toString(array));<br>    
+}<br>
+}<br>
