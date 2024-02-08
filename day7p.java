6b3dd4905107e1cc552fcde166a9cce39759cc6f
sneha2085 <147432777+sneha2085@users.noreply.github.com>
2024-02-08 10:25:00 +0530

    Update

---
+++ day7p.java
@@ -0,0 +1,13 @@
+class findsum <br>
+{<br>   
+ public static void main(String[] args)<br>    
+{ <br>   
+ int sum =0;<br>      
+  int i;<br>      
+  int[] arr ={2,4,6,4};<br>        for(i=0;i<4;i++)
+        {<br>           
+sum =sum+arr[i];<br>       
+ }<br>      
+  System.out.println("the sum is: "+sum);<br>        
+    }<br>
+}<br>
